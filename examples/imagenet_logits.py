029580804c5f93dd7406fbc4ceb5d5289c7de241
Alex Andonian <alexandonian@gmail.com>
2018-02-12 03:29:51 -0500

    Fix imagenet logits example

--- examples/imagenet_logits.py
+++ examples/imagenet_logits.py
@@ -21,51 +21,50 @@ parser.add_argument('--arch', '-a', metavar='ARCH', default='nasnetalarge',
                         ' | '.join(model_names) +
                         ' (default: nasnetalarge)',
                     nargs='+')
-parser.add_argument('--path_img', type=str, default='data/cat.jpg')
+parser.add_argument('--path_img', type=str, default='../data/cat.jpg')
 
 def main():
     global args
     args = parser.parse_args()
 
-    for arch in args.arch:
-        # Load Model
-        model = pretrainedmodels.__dict__[arch](num_classes=1000,
-                                                pretrained='imagenet')
-        model.eval()
+    # Load Model
+    model = pretrainedmodels.__dict__[args.arch](num_classes=1000,
+                                            pretrained='imagenet')
+    model.eval()
 
-        path_img = args.path_img
-        # Load and Transform one input image
-        load_img = utils.LoadImage()
-        tf_img = utils.TransformImage(model)
+    path_img = args.path_img
+    # Load and Transform one input image
+    load_img = utils.LoadImage()
+    tf_img = utils.TransformImage(model)
 
-        input_data = load_img(args.path_img) # 3x400x225
-        input_data = tf_img(input_data)      # 3x299x299
-        input_data = input_data.unsqueeze(0) # 1x3x299x299
-        input = torch.autograd.Variable(input_data)
+    input_data = load_img(args.path_img) # 3x400x225
+    input_data = tf_img(input_data)      # 3x299x299
+    input_data = input_data.unsqueeze(0) # 1x3x299x299
+    input = torch.autograd.Variable(input_data)
 
-        # Load Imagenet Synsets
-        with open('data/imagenet_synsets.txt', 'r') as f:
-            synsets = f.readlines()
+    # Load Imagenet Synsets
+    with open('../data/imagenet_synsets.txt', 'r') as f:
+        synsets = f.readlines()
 
-        # len(synsets)==1001
-        # sysnets[0] == background
-        synsets = [x.strip() for x in synsets]
-        splits = [line.split(' ') for line in synsets]
-        key_to_classname = {spl[0]:' '.join(spl[1:]) for spl in splits}
+    # len(synsets)==1001
+    # sysnets[0] == background
+    synsets = [x.strip() for x in synsets]
+    splits = [line.split(' ') for line in synsets]
+    key_to_classname = {spl[0]:' '.join(spl[1:]) for spl in splits}
 
-        with open('data/imagenet_classes.txt', 'r') as f:
-            class_id_to_key = f.readlines()
+    with open('../data/imagenet_classes.txt', 'r') as f:
+        class_id_to_key = f.readlines()
 
-        class_id_to_key = [x.strip() for x in class_id_to_key]
+    class_id_to_key = [x.strip() for x in class_id_to_key]
 
-        # Make predictions
-        output = model(input) # size(1, 1000)
-        max, argmax = output.data.squeeze().max(0)
-        class_id = argmax[0]
-        class_key = class_id_to_key[class_id]
-        classname = key_to_classname[class_key]
+    # Make predictions
+    output = model(input) # size(1, 1000)
+    max, argmax = output.data.squeeze().max(0)
+    class_id = argmax[0]
+    class_key = class_id_to_key[class_id]
+    classname = key_to_classname[class_key]
 
-        print("'{}': '{}' is a '{}'".format(arch, path_img, classname))
+    print("'{}': '{}' is a '{}'".format(args.arch, path_img, classname))
 
 if __name__ == '__main__':
     main()
