4e80d0ac5dee130836371c3aa8b2a88c56aa252f
Alex Andonian <alexandonian@gmail.com>
2019-11-28 06:22:33 -0500

    Move util classes to util module

--- pretorched/runners/utils.py
+++ pretorched/runners/utils.py
@@ -25,12 +25,12 @@ class AverageMeter(object):
 
 
 class ProgressMeter(object):
-    def __init__(self, num_batches, *meters, prefix=""):
+    def __init__(self, num_batches, meters, prefix=""):
         self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
         self.meters = meters
         self.prefix = prefix
 
-    def print(self, batch):
+    def display(self, batch):
         entries = [self.prefix + self.batch_fmtstr.format(batch)]
         entries += [str(meter) for meter in self.meters]
         print('\t'.join(entries))
