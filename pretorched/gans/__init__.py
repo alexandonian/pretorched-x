7eddc0722fd82e71c865d1006054530a1386560e
Alex Andonian <alexandonian@gmail.com>
2019-12-02 00:48:43 -0500

    Add pretrained proggans

--- pretorched/gans/__init__.py
+++ pretorched/gans/__init__.py
@@ -4,12 +4,14 @@ from . import sngan
 from . import utils
 from .biggan import BigGAN
 from .stylegan import stylegan
+from .proggan import proggan
 
 __all__ = [
     'BigGAN',
     'dcgan',
     'sngan',
     'sagan',
+    'proggan',
     'stylegan',
     'utils',
 ]
