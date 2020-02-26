7fa32ee9a922182bdc73769475be48600db5d947
Alex Andonian <alexandonian@gmail.com>
2020-02-25 21:49:25 -0500

    Update tests

--- tests/runners/test_core.py
+++ tests/runners/test_core.py
@@ -23,7 +23,7 @@ def test_get_dataset(name, split, size, dataset_type):
                                split=split, size=224,
                                dataset_type=dataset_type)
     img, label = dataset[0]
-    assert label < cfg.num_classes_dict[name]
+    assert label < cfg.NUM_CLASSES[name]
     assert img.shape == torch.Size((3, size, size))
 
 
@@ -39,7 +39,7 @@ def test_get_hybrid_dataset(name, split, size, dataset_type):
                                       split=split, size=224,
                                       dataset_type=dataset_type)
     img, label = dataset[0]
-    assert label < cfg.num_classes_dict[name]
+    assert label < cfg.NUM_CLASSES[name]
     assert img.shape == torch.Size((3, size, size))
 
 
@@ -66,34 +66,46 @@ def test_get_dataloader(name, split, size, dataset_type):
         assert x.shape == torch.Size((BATCH_SIZE, 3, size, size))
 
 
-@pytest.mark.parametrize('name, split, segment_count, size, dataset_type', [
-    ('Moments', 'train', 16, 224, 'VideoRecordDataset'),
-    ('Moments', 'val', 16, 224, 'VideoRecordDataset'),
+@pytest.mark.parametrize('name, split, segment_count, size, dataset_type, record_set_type', [
+    ('Moments', 'train', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('Moments', 'val', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('Moments', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('Moments', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('MultiMoments', 'train', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'val', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'train', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'val', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
 ])
-def test_get_video_dataset(name, split, segment_count, size, dataset_type):
+def test_get_video_dataset(name, split, segment_count, size, dataset_type, record_set_type):
 
     dataset = core.get_video_dataset(
         name, data_root=DATA_ROOT, split=split, size=size, dataset_type=dataset_type,
-        load_in_mem=False, segment_count=segment_count)
+        record_set_type=record_set_type, load_in_mem=False, segment_count=segment_count)
 
     for i, (x, y) in enumerate(dataset):
         if i >= MAX_ITERS:
             break
 
-        assert y < cfg.num_classes_dict[name]
+        assert y < cfg.NUM_CLASSES[name]
         assert x.shape == torch.Size((3, segment_count, size, size))
 
 
-@pytest.mark.parametrize('name, split, segment_count, size, dataset_type', [
-    ('Moments', 'train', 16, 224, 'VideoRecordDataset'),
-    ('Moments', 'val', 16, 224, 'VideoRecordDataset'),
+@pytest.mark.parametrize('name, split, segment_count, size, dataset_type, record_set_type', [
+    ('Moments', 'train', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('Moments', 'val', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('Moments', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('Moments', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('MultiMoments', 'train', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'val', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'train', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'val', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
 ])
-def test_get_video_dataloader(name, split, segment_count, size, dataset_type):
+def test_get_video_dataloader(name, split, segment_count, size, dataset_type, record_set_type):
 
     loader = core.get_video_dataloader(
         name, data_root=DATA_ROOT, split=split, size=size, dataset_type=dataset_type,
-        batch_size=BATCH_SIZE, num_workers=8, shuffle=True, load_in_mem=False, pin_memory=True,
-        drop_last=False, distributed=False, segment_count=segment_count)
+        record_set_type=record_set_type, batch_size=BATCH_SIZE, num_workers=8, shuffle=True,
+        load_in_mem=False, pin_memory=True, drop_last=False, distributed=False, segment_count=segment_count)
 
     for i, (x, y) in enumerate(loader):
         if i >= MAX_ITERS:
