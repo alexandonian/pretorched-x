f2cbb5d4dda42961832b615d9460a926e129f35a
Alex Andonian <alexandonian@gmail.com>
2020-05-12 04:38:51 -0400

    Update tests

--- tests/runners/test_core.py
+++ tests/runners/test_core.py
@@ -8,6 +8,12 @@ DATA_ROOT = cfg.DATA_ROOT
 MAX_ITERS = 10
 BATCH_SIZE = 32
 
+SKIP_VIDEO = False
+SKIP_VIDEO_DATASET = False
+SKIP_VIDEO_ZIP_DATASET = False
+SKIP_DATASET = False
+SKIP_DATALOADER = True
+
 
 @pytest.mark.parametrize('name, split, size, dataset_type', [
     ('ImageNet', 'train', 224, 'ImageFolder'),
@@ -43,6 +49,7 @@ def test_get_hybrid_dataset(name, split, size, dataset_type):
     assert img.shape == torch.Size((3, size, size))
 
 
+@pytest.mark.skipif(SKIP_DATALOADER, reason='Not needed.')
 @pytest.mark.parametrize('name, split, size, dataset_type', [
     ('ImageNet', 'train', 224, 'ImageFolder'),
     ('ImageNet', 'val', 224, 'ImageFolder'),
@@ -66,25 +73,17 @@ def test_get_dataloader(name, split, size, dataset_type):
         assert x.shape == torch.Size((BATCH_SIZE, 3, size, size))
 
 
+@pytest.mark.skipif(SKIP_VIDEO_DATASET, reason='Not needed')
 @pytest.mark.parametrize('name, split, segment_count, size, dataset_type, record_set_type', [
     ('Moments', 'train', 16, 224, 'VideoRecordDataset', 'RecordSet'),
     ('Moments', 'val', 16, 224, 'VideoRecordDataset', 'RecordSet'),
     ('Moments', 'test', 16, 224, 'VideoRecordDataset', 'RecordSet'),
-    ('Moments', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
-    ('Moments', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
-    ('Moments', 'test', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
     ('MultiMoments', 'train', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
     ('MultiMoments', 'val', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
-    # ('MultiMoments', 'test', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
-    ('MultiMoments', 'train', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
-    # ('MultiMoments', 'test', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'test', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
     ('Kinetics', 'train', 16, 224, 'VideoRecordDataset', 'RecordSet'),
     ('Kinetics', 'val', 16, 224, 'VideoRecordDataset', 'RecordSet'),
-    # ('Kinetics', 'test', 16, 224, 'VideoRecordDataset', 'RecordSet'),
-    ('Kinetics', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
-    ('Kinetics', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
-    # ('Kinetics', 'test', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
-
+    ('Kinetics', 'test', 16, 224, 'VideoRecordDataset', 'RecordSet'),
 ])
 def test_get_video_dataset(name, split, segment_count, size, dataset_type, record_set_type):
 
@@ -100,27 +99,70 @@ def test_get_video_dataset(name, split, segment_count, size, dataset_type, recor
         assert x.shape == torch.Size((3, segment_count, size, size))
 
 
+@pytest.mark.skipif(SKIP_VIDEO_ZIP_DATASET, reason='Not needed')
+@pytest.mark.parametrize('name, split, segment_count, size, dataset_type, record_set_type', [
+    ('Moments', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('Moments', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('Moments', 'test', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('MultiMoments', 'train', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'val', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
+    ('Kinetics', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('Kinetics', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('Kinetics', 'test', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+])
+def test_get_video_zip_dataset(name, split, segment_count, size, dataset_type, record_set_type):
+
+    dataset = core.get_video_dataset(
+        name, data_root=DATA_ROOT, split=split, size=size, dataset_type=dataset_type,
+        record_set_type=record_set_type, load_in_mem=False, segment_count=segment_count)
+
+    for i, (x, y) in enumerate(dataset):
+        if i >= MAX_ITERS:
+            break
+
+        assert y < cfg.NUM_CLASSES[name]
+        assert x.shape == torch.Size((3, segment_count, size, size))
+
+
+@pytest.mark.skipif(SKIP_DATALOADER, reason='Not needed.')
 @pytest.mark.parametrize('name, split, segment_count, size, dataset_type, record_set_type', [
     ('Moments', 'train', 16, 224, 'VideoRecordDataset', 'RecordSet'),
     ('Moments', 'val', 16, 224, 'VideoRecordDataset', 'RecordSet'),
     ('Moments', 'test', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('MultiMoments', 'train', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'val', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
+    ('MultiMoments', 'test', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
+    ('Kinetics', 'train', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('Kinetics', 'val', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('Kinetics', 'test', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+])
+def test_get_video_dataloader(name, split, segment_count, size, dataset_type, record_set_type):
+
+    loader = core.get_video_dataloader(
+        name, data_root=DATA_ROOT, split=split, size=size, dataset_type=dataset_type,
+        record_set_type=record_set_type, batch_size=BATCH_SIZE, num_workers=8, shuffle=True,
+        load_in_mem=False, pin_memory=True, drop_last=False, distributed=False, segment_count=segment_count)
+
+    for i, (x, y) in enumerate(loader):
+        if i >= MAX_ITERS:
+            break
+
+        assert y.shape == torch.Size((BATCH_SIZE,))
+        assert x.shape == torch.Size((BATCH_SIZE, 3, segment_count, size, size))
+
+@pytest.mark.skipif(SKIP_DATALOADER, reason='Not needed.')
+@pytest.mark.parametrize('name, split, segment_count, size, dataset_type, record_set_type', [
     ('Moments', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
     ('Moments', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
     ('Moments', 'test', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
-    ('MultiMoments', 'train', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
-    ('MultiMoments', 'val', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
-    # ('MultiMoments', 'test', 16, 224, 'VideoRecordDataset', 'MultiLabelRecordSet'),
     ('MultiMoments', 'train', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
     ('MultiMoments', 'val', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
-    # ('MultiMoments', 'test', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
-    ('Kinetics', 'train', 16, 224, 'VideoRecordDataset', 'RecordSet'),
-    ('Kinetics', 'val', 16, 224, 'VideoRecordDataset', 'RecordSet'),
-    # ('Kinetics', 'test', 16, 224, 'VideoRecordDataset', 'RecordSet'),
+    ('MultiMoments', 'test', 16, 224, 'VideoRecordZipDataset', 'MultiLabelRecordSet'),
     ('Kinetics', 'train', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
     ('Kinetics', 'val', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
-    # ('Kinetics', 'test', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
+    ('Kinetics', 'test', 16, 224, 'VideoRecordZipDataset', 'RecordSet'),
 ])
-def test_get_video_dataloader(name, split, segment_count, size, dataset_type, record_set_type):
+def test_get_video_zip_dataloader(name, split, segment_count, size, dataset_type, record_set_type):
 
     loader = core.get_video_dataloader(
         name, data_root=DATA_ROOT, split=split, size=size, dataset_type=dataset_type,
