7eeda02f67f8c2f189affc5217f3dab1bd147829
Alex Andonian <alexandonian@gmail.com>
2021-02-15 03:59:29 -0500

    Add try catch for stylegan2 import in case cuda ops cannot be compiled

--- pretorched/gans/__init__.py
+++ pretorched/gans/__init__.py
@@ -4,12 +4,19 @@ from . import sngan
 from . import utils
 from .biggan import BigGAN
 from .stylegan import stylegan
-from .stylegan2 import stylegan2
 from .proggan import proggan
 from .biggan_deep import BigGANDeep
 
+try:
+    from .stylegan2 import stylegan2
+except RuntimeError:
+    stylegan2 = None
+    print('Warning: could not compile cuda code for stylegan2')
+    print('Ensure $CUDA_HOME/bin/nvcc exists!')
+
 __all__ = [
     'BigGAN',
+    'BigGANDeep',
     'dcgan',
     'sngan',
     'sagan',
